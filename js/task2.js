cffbedb14dd8249f460271c0851baa27bbd41b55
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2020-11-07 19:02:50 +0200

    hw-11-2

--- js/task2.js
+++ js/task2.js
@@ -14,9 +14,15 @@ const users = [
 // };
 
 const toggleUserState = (allUsers, userName) => {
-    return new Promise(resolve => {resolve (allUsers.map(user =>
-            user.name === userName ? { ...user, active: !user.active } : user)) 
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  resolve(updatedUsers);
     });
+  
+    
 }
 
 const logger = updatedUsers => console.table(updatedUsers);
